333e12e63ac8f43619c71f665256d15cb598cc1f
Tadeo-Ibaceta <tadeoezequielibaceta@impatrq.com>
2024-11-20 13:32:28 -0300

    fix: lectura de termocuplas simultaneamente

--- Software Rev-Control/rev-control_spi.c
+++ Software Rev-Control/rev-control_spi.c
@@ -8,40 +8,67 @@
 #include "fsl_spi.h"
 
 // Pines de Slave Output (MISO)
-#define SO		20
+#define SO		17
 // Pin de clock
-#define SCK		10
+#define SCK		18
 // Pines de Chip Select
-#define CSTCAA	1
-#define CSTCAB	0
-#define CSTCO 	4
-#define CSTC1 	18
+//#define CSTCAA	1
+//#define CSTCAB	0
+//#define CSTCO 	4
+#define CSTC1 	10
 #define CSTC2 	19
-#define CSTC3 	17
+#define CSTC3 	20
 #define CSTC4	13
 
 //Array de todos los Chip Select
-uint8_t CS[7] = {CSTCAA, CSTCAB, CSTCO, CSTC1, CSTC2, CSTC3, CSTC4};
-
+uint16_t temp[4] = {};
 //Contador
 int t;
-/**
- * @brief Pone el CS en low
- */
-void spi_cs_low(void) {
-    asm volatile("nop \n nop \n nop");
-    GPIO_PinWrite(GPIO, 0, CS[t], 0);
-    asm volatile("nop \n nop \n nop");
+
+void TC1_read(spi_transfer_t *xfer){
+	// Tiro abajo CS
+	GPIO_PinWrite(GPIO, 0, CSTC1, 0);
+	asm volatile("nop \n nop \n nop");
+	// Hace una lectura del bus
+	SPI_MasterTransferBlocking(SPI0, xfer);
+	// Deshabilito CS
+	GPIO_PinWrite(GPIO, 0, CSTC1, 1);
+	asm volatile("nop \n nop \n nop");
 }
 
-/**
- * @brief Pone el CS en high
- */
-void spi_cs_high(void) {
-    asm volatile("nop \n nop \n nop");
-    GPIO_PinWrite(GPIO, 0, CS[t], 1);
-    asm volatile("nop \n nop \n nop");
+void TC2_read(spi_transfer_t *xfer){
+	// Tiro abajo CS
+	GPIO_PinWrite(GPIO, 0, CSTC2, 0);
+	asm volatile("nop \n nop \n nop");
+	// Hace una lectura del bus
+	SPI_MasterTransferBlocking(SPI0, xfer);
+	// Deshabilito CS
+	GPIO_PinWrite(GPIO, 0, CSTC2, 1);
+	asm volatile("nop \n nop \n nop");
 }
+
+void TC3_read(spi_transfer_t *xfer){
+	// Tiro abajo CS
+	GPIO_PinWrite(GPIO, 0, CSTC3, 0);
+	asm volatile("nop \n nop \n nop");
+	// Hace una lectura del bus
+	SPI_MasterTransferBlocking(SPI0, xfer);
+	// Deshabilito CS
+	GPIO_PinWrite(GPIO, 0, CSTC3, 1);
+	asm volatile("nop \n nop \n nop");
+}
+
+void TC4_read(spi_transfer_t *xfer){
+	// Tiro abajo CS
+	GPIO_PinWrite(GPIO, 0, CSTC4, 0);
+	asm volatile("nop \n nop \n nop");
+	// Hace una lectura del bus
+	SPI_MasterTransferBlocking(SPI0, xfer);
+	// Deshabilito CS
+	GPIO_PinWrite(GPIO, 0, CSTC4, 1);
+	asm volatile("nop \n nop \n nop");
+}
+
 
 /**
  * @brief Hace una lectura de temperatura
@@ -57,12 +84,20 @@ float max6675_get_temp(void) {
 			.dataSize = 2,		// Dos bytes se tienen que leer
 			.configFlags = kSPI_EndOfTransfer | kSPI_EndOfFrame
 	};
-	// Tiro abajo CS
-    spi_cs_low();
-    // Hace una lectura del bus
-    SPI_MasterTransferBlocking(SPI0, &xfer);
-    // Deshabilito CS
-    spi_cs_high();
+	if(t == 0){
+		TC1_read(&xfer);
+	}
+	else if(t == 1){
+		TC2_read(&xfer);
+	}
+	else if(t == 2){
+		TC3_read(&xfer);
+	}
+	else if(t == 3){
+		TC4_read(&xfer);
+	}
+
+
     // Armo los 16 bits
 	uint16_t reading = (buffer[0] << 8) + buffer[1];
     // Los primeros tres bits no sirven
@@ -89,13 +124,13 @@ int main(void) {
 
     // Habilito el pin de Chip Select como salida
     GPIO_PortInit(GPIO, 0);
-    GPIO_PinInit(GPIO, 0, CSTCAA, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
-    GPIO_PinInit(GPIO, 0, CSTCAB, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
-    GPIO_PinInit(GPIO, 0, CSTCO, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
+    //GPIO_PinInit(GPIO, 0, CSTCAA, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
+    //GPIO_PinInit(GPIO, 0, CSTCAB, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
+    // GPIO_PinInit(GPIO, 0, CSTCO, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
     GPIO_PinInit(GPIO, 0, CSTC1, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
     GPIO_PinInit(GPIO, 0, CSTC2, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
-    GPIO_PinInit(GPIO, 0, CSTC3, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
-    GPIO_PinInit(GPIO, 0, CSTC4, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
+    //GPIO_PinInit(GPIO, 0, CSTC3, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
+    //GPIO_PinInit(GPIO, 0, CSTC4, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
 
     // Clock para SPI
     CLOCK_Select(kSPI0_Clk_From_MainClk);
@@ -114,12 +149,23 @@ int main(void) {
     SPI_MasterInit(SPI0, &config, SystemCoreClock);
 
     while(1) {
-    	for(t = 0; t < 7; t++){
+    	for(t = 0; t < 4; t++){
     		// Leo la temperatura de la termocupla
-    		float temp = max6675_get_temp();
+    		temp[t] = max6675_get_temp();
     		// Muestro como entero
-    		if (temp < 300){
-    			PRINTF("La temperatura fue: %d\n", (uint16_t)temp);
+    		if (temp[t] < 300){
+    			if (t == 0){
+    				PRINTF("La temperatura en la termocupla 1 fue: %d\n", (uint16_t)temp[0]);
+    			}
+    			else if (t == 1){
+    				PRINTF("La temperatura en la termocupla 2 fue: %d\n", (uint16_t)temp[1]);
+    			}
+    			else if (t == 2){
+    				PRINTF("La temperatura en la termocupla 3 fue: %d\n", (uint16_t)temp[2]);
+    			}
+    			else if (t == 3){
+    				PRINTF("La temperatura en la termocupla 4 fue: %d\n", (uint16_t)temp[3]);
+    			}
     		}
     		// Demora
     		for(uint32_t i = 0; i < 1000000; i++);

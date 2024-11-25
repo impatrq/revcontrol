a4260054816c928bf43d65f89fde021609d90c7d
Tadeo-Ibaceta <tadeoezequielibaceta@impatrq.com>
2024-11-25 11:37:42 -0300

    fix: arreglo alertas de O2 y presion

--- Software Rev-Control/rev-control_adc.c
+++ Software Rev-Control/rev-control_adc.c
@@ -59,8 +59,10 @@ int main(void) {
 
     CLOCK_EnableClock(kCLOCK_Swm);
 
-    SWM_SetFixedPinSelect(SWM0, kSWM_ADC_CHN1, true); //----------PIO0_6---------// %O2
-    SWM_SetFixedPinSelect(SWM0, kSWM_ADC_CHN3, true); //----------PIO0_23--------// PRESS OIL
+    SWM_SetFixedPinSelect(SWM0, kSWM_ADC_CHN1, true);   //----------PIO0_6---------// %O2
+    SWM_SetFixedPinSelect(SWM0, kSWM_ADC_CHN3, true);   //----------PIO0_23--------// PRESS OIL
+    GPIO_PinInit(GPIO, 0, O2_Alert, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
+    GPIO_PinInit(GPIO, 0, Pressure_Alert, &(gpio_pin_config_t){ kGPIO_DigitalOutput, 1 });
 
     CLOCK_DisableClock(kCLOCK_Swm);
 
